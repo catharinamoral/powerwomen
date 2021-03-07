d9a3f42809642bb3660a38c8304070682e187495
Catharina <catharina.moral@gmail.com>
2021-03-07 19:06:53 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/powerwomen/PowerwomenApplication.java
@@ -0,0 +1,13 @@
+package br.com.powerwomen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerwomenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerwomenApplication.class, args);
+	}
+
+}
